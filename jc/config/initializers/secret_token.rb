990c3e21885a5aed247be79036462e6240f579dc
Wu Yang <wuyangnju@gmail.com>
2012-12-13 20:19:06 +0800

    rails new jc; import into eclipse

---
+++ jc/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jc::Application.config.secret_token = 'ec4558a61754063ce94e2efe85a91965285ada879e1ec50539e44a8691a308f76edc493f209ae8c27e4265d93c68a9b14635858ca726917dd3de5ed019b973c7'

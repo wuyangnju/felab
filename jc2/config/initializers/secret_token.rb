cce281865a64672115d00c47bb995460ac7b758c
godfrey90 <gaoshengnju@gmail.com>
2012-12-28 14:48:25 +0800

    rails new jc2

---
+++ jc2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jc2::Application.config.secret_token = '8d1efa3455dd315eb21c606eb7a675530784d1abccb2c58c2796039add6d5f6cf814162fc08059cef078896dfabd9fdeae3d29cb6bec31bb5fe0faa2ec645aa5'

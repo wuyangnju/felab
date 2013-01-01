ec17c62d61ed972be9906c82479eca489582a387
Wu Yang <wuyangnju@gmail.com>
2013-01-01 19:59:19 +0800

    add search by name

--- jc2/app/controllers/authors_controller.rb
+++ jc2/app/controllers/authors_controller.rb
@@ -3,6 +3,7 @@ class AuthorsController < ApplicationController
   def index
     @authors = Author.scoped;
     @authors = @authors.where(:org_id => params[:org_id]) unless params[:org_id].blank?
+    @authors = @authors.where(:name => params[:name].strip) unless params[:name].blank?
     @authors = @authors.paginate(:page => params[:page]);
 
     render :json => @authors;
@@ -10,7 +11,7 @@ class AuthorsController < ApplicationController
 
   def show
     @author = Author.find(params[:id])
-
+    @author.
     render :json => @author;
   end
 end
--- jc2/app/controllers/orgs_controller.rb
+++ jc2/app/controllers/orgs_controller.rb
@@ -1,7 +1,9 @@
 class OrgsController < ApplicationController
 
   def index
-    @orgs = Org.paginate(:page => params[:page]);
+    @orgs = Org.scoped;
+    @orgs = @orgs.where(:name => params[:name].strip) unless params[:name].blank?
+    @orgs = @orgs.paginate(:page => params[:page]);
 
     render :json => @orgs;
   end
